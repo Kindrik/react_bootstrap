c71fcebfdd7134b75d5e474e33c16d99356f83a5
Kindrik <winterstorm_11@hotmail.com>
2021-08-27 21:27:16 -0700

    added bootstrap 5 via sass, and started playing around with bootstrap 5 grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row p-5">
+          <div className="col p-3 mb-2 bg-light text-dark border">Column 1</div>
+          <div className="col p-3 mb-2 bg-light text-dark border">Column 2</div>
+          <div className="col p-3 mb-2 bg-light text-dark border">Column 3</div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "../src/styles.scss";
 
 ReactDOM.render(
   <React.StrictMode>
