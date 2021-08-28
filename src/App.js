4ac2775e7a567d364688ea2ff8d481dc2f8680db
Kindrik <winterstorm_11@hotmail.com>
2021-08-28 14:51:44 -0700

    rollback on bootstrap 5, using the JS in it is too much of a pain. Switching to material UI

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <div className="row p-5">
-          <div className="col p-3 mb-2 bg-light text-dark border">Column 1</div>
-          <div className="col p-3 mb-2 bg-light text-dark border">Column 2</div>
-          <div className="col p-3 mb-2 bg-light text-dark border">Column 3</div>
-        </div>
-      </div>
+
     </div>
   );
 }
